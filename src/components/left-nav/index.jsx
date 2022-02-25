241c14c55722060557bae2c9ccc639e85a38bbe3
Evan-LZM <zliu246@aucklanduni.ac.nz>
2022-02-26 07:46:42 +1300

    Dynamic Show Up The Admin Menu
    Dynamic show up the admin menu

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -3,18 +3,7 @@ import './index.less';
 import logo from "../../assets/images/init_logo2.png";
 import {Link} from 'react-router-dom';
 import { Menu, Button,Layout } from 'antd';
-import {
-  BarChartOutlined,
-  PieChartOutlined,
-  LineChartOutlined,
-  MailOutlined,
-  UserOutlined,
-  ShoppingCartOutlined,
-  HomeOutlined,
-  AppstoreOutlined,
-  ShoppingOutlined,
-  ProfileOutlined,
-} from '@ant-design/icons';
+import menuList from '../../config/menuConfig'
 
 const {Sider} = Layout;
 const { SubMenu } = Menu;
@@ -23,6 +12,28 @@ export default class LeftNav extends Component{
     state={
         collapes:false
     };
+
+    getMenuNodes=(menuList)=>{
+        return menuList.map(item=>{
+            if (!item.children){
+                return(
+                    <Menu.Item icon={item.icon} key={item.key} >
+                        <Link to={item.path}>
+                            {item.title}
+                        </Link>
+                    </Menu.Item>
+                )
+            }else{
+                return(
+                    <SubMenu icon={item.icon} title={item.title} key={item.key}>
+                        {
+                            this.getMenuNodes(item.children)
+                        }
+                    </SubMenu>
+                )
+            }
+        })
+    }
 
     onCollapse = collapsed => {
       this.setState({ collapsed });
@@ -45,50 +56,7 @@ export default class LeftNav extends Component{
               theme="dark"
               inlineCollapsed={this.state.collapsed}
             >
-              <Menu.Item key="1" icon={<HomeOutlined/>}>
-                  <Link to='/admin/home'>
-                  Dashboard
-                  </Link>
-              </Menu.Item>
-              <SubMenu key="2" icon={<AppstoreOutlined />} title="Shop">
-                <Menu.Item key="5" icon={<ProfileOutlined/>}>
-                  <Link to='/admin/category'>
-                  Category
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="6" icon={<ShoppingOutlined/>}>
-                  <Link to='/admin/product'>
-                  Product
-                  </Link>
-                </Menu.Item>
-              </SubMenu>
-              <Menu.Item key="3" icon={<UserOutlined />}>
-                  <Link to='/admin/user'>
-                  User
-                  </Link>
-              </Menu.Item>
-              <Menu.Item key="4" icon={<ShoppingCartOutlined />}>
-                  <Link to='/admin/order'>
-                  Orders
-                  </Link>
-              </Menu.Item>
-              <SubMenu key="7" icon={<BarChartOutlined />} title="Chart ">
-                <Menu.Item key="8" icon={<BarChartOutlined/>}>
-                  <Link to='/admin/bar'>
-                      Bar
-                  </Link>
-                </Menu.Item>
-                  <Menu.Item key="9" icon={<PieChartOutlined/>}>
-                  <Link to='/admin/pie'>
-                      Pie
-                  </Link>
-                </Menu.Item>
-                  <Menu.Item key="10" icon={<LineChartOutlined />}>
-                  <Link to='/admin/line'>
-                      Line
-                  </Link>
-                </Menu.Item>
-              </SubMenu>
+              {this.getMenuNodes(menuList)}
             </Menu>
             </Sider>
         )
---
+++ src/config/menuConfig.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import {
+  BarChartOutlined,
+  PieChartOutlined,
+  LineChartOutlined,
+  MailOutlined,
+  UserOutlined,
+  ShoppingCartOutlined,
+  HomeOutlined,
+  AppstoreOutlined,
+  ShoppingOutlined,
+  ProfileOutlined,
+} from '@ant-design/icons';
+
+const menuList=[
+  {
+    title:'Dashboard',
+    path:'/admin/home',
+    key:'1',
+    icon: <HomeOutlined/>
+  },
+  {
+    title:'Shop',
+    path:'/',
+    key:'2',
+    icon: <AppstoreOutlined/>,
+    children:[
+        {
+          title:'Category',
+          path:'/admin/category',
+          key:'3',
+          icon: <ProfileOutlined/>
+        },
+        {
+          title:'Product',
+          key:'4',
+          path:'/admin/product',
+          icon: <ShoppingOutlined/>
+        }
+    ]
+  },
+  {
+    title:'User',
+    path:'/admin/user',
+    key:'5',
+    icon: <UserOutlined/>
+  },
+  {
+    title:'Order',
+    path:'/admin/order',
+    key:'6',
+    icon: <ShoppingCartOutlined/>
+  },
+  {
+    title:'Chart',
+    path:'/',
+    key:'7',
+    icon: <BarChartOutlined/>,
+    children:[
+        {
+          title:'Bar',
+          path:'/admin/bar',
+          key:'8',
+          icon: <BarChartOutlined/>
+        },
+        {
+          title:'Pie',
+          key:'9',
+          path:'/admin/pie',
+          icon: <PieChartOutlined/>
+        },
+        {
+          title:'Line',
+          key:'10',
+          path:'/admin/line',
+          icon: <LineChartOutlined/>
+        }
+    ]
+  }
+]
+
+export default menuList
