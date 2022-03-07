d66da8ce3bc8110c5407041b4dcba464e29e515c
Evan-LZM <zliu246@aucklanduni.ac.nz>
2022-03-08 00:37:39 +1300

    Add Product Home Static UI
    Add product home static ui

--- src/pages/product/home.jsx
+++ src/pages/product/home.jsx
@@ -1,12 +1,80 @@
-import React,{Component} from 'react'
+import React,{useEffect,useState} from 'react'
+import {Card,Select,Input,Button,Icon,Table} from 'antd'
+import {PlusOutlined} from '@ant-design/icons';
 
-export default class ProductHome extends Component{
-    render(){
-        return(
-            <div>
-                Product Home
-            </div>
-        )
+const Option=Select.Option
+
+export default function ProductHome(){
+    const [columns,setColumns]=useState([])
+
+    const dataSource = [
+      {
+        key: '1',
+        name: 'Mike',
+        age: 32,
+        address: '10 Downing Street',
+      },
+      {
+        key: '2',
+        name: 'John',
+        age: 42,
+        address: '10 Downing Street',
+      },
+    ];
+    
+
+    const initColumns=()=>{
+        const columns = [
+          {
+            title: 'Product Name(SPU)',
+            dataIndex: 'name',
+            key: 'name',
+          },
+          {
+            title: 'Description',
+            dataIndex: 'age',
+            key: 'age',
+          },
+          {
+            title: 'Status',
+            dataIndex: 'address',
+            key: 'address',
+          },
+          {
+            title: 'Action',
+            dataIndex: 'address',
+            key: 'address',
+          },
+        ];
+        setColumns(columns)
     }
 
+    useEffect(()=>{
+        initColumns()
+    },[])
+    const title=(
+        <span>
+            <Select value='0'>
+                <Option value='0'>Search By Name</Option>
+                <Option value='1'>Search By Details</Option>
+            </Select>
+            <Input placeholder="keywords" style={{width:150,margin:'0 10px'}}/>
+            <Button type='primary'>Search</Button>
+        </span>
+    )
+
+    const extra=(
+        <Button type='primary' icon={<PlusOutlined/>}>
+            Add Product
+        </Button>
+    )
+
+    return(
+        <Card title={title} extra={extra}>
+            <Table 
+                dataSource={dataSource}
+                columns={columns}
+            />
+        </Card>
+    )
 }
