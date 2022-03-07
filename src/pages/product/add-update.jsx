e6bdb652f2d6f4870a9f11fa19f8a98999f512c0
Evan-LZM <zliu246@aucklanduni.ac.nz>
2022-03-07 23:40:27 +1300

    Config Product Router
    Config product router
    
    ID: #25

---
+++ src/pages/product/add-update.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+
+export default class ProductAddUpdate extends Component{
+    render(){
+        return(
+            <div>
+                Product Add Update
+            </div>
+        )
+    }
+
+}
