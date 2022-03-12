ca675ec2727c127a5ccfabe1b23b56d05bc93ba3
Evan-LZM <zliu246@aucklanduni.ac.nz>
2022-03-12 22:02:28 +1300

    Create Spu Using Drawer
    Create spu using drawer
    
    ID: #31

--- src/pages/product/add-update.jsx
+++ src/pages/product/add-update.jsx
@@ -1,12 +1,72 @@
-import React,{Component} from 'react'
+import React, { useState } from 'react';
+import {
+  Form,
+  Input,
+  InputNumber,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete,
+} from 'antd';
 
-export default class ProductAddUpdate extends Component{
-    render(){
-        return(
-            <div>
-                Product Add Update
-            </div>
-        )
+const Item=Form.Item;
+
+const Option=Select.Option;
+
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 4,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 6,
+    },
+  },
+};
+
+export default function ProductAddUpdate(){
+    const [form]=Form.useForm()
+    const onFinish=(vales)=>{
+        console.log("form values:",values)
     }
+    return (
+      <Form 
+        {...formItemLayout}
+        form={form} 
+        name="editspu" 
+        onFinish={onFinish}
+        scrollToFirstError
+      >
+        <Item name="Name" 
+          label="Name" 
+          tooltip="SPU Name"
+          rules={[{required:true,message:"Please input spu name",whitespace:true,}]}
+        >
+            <Input/>
+        </Item>
 
-}
+        <Item
+            name="category"
+            label="category"
+        >
+            <Select placeholder="select your gender">
+              <Option value="male">Male</Option>
+              <Option value="female">Female</Option>
+              <Option value="other">Other</Option>
+            </Select>
+        </Item>
+         
+      </Form>
+    );
+};
---
+++ src/pages/product/spu-edit-form.jsx
@@ -0,0 +1,203 @@
+import React, { useState,useEffect } from 'react';
+import {
+  Form,
+  Input,
+  InputNumber,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete,
+  Space,
+} from 'antd';
+import {PlusOutlined,MinusCircleOutlined} from '@ant-design/icons';
+import {
+        reqCategories,
+        reqSecCategories,
+        reqThirdCategories
+        } from '../../api'
+
+const Item=Form.Item;
+
+const Option=Select.Option;
+
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 5,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 18,
+    },
+  },
+};
+
+export default function SpuEditForm(props){
+    const children = [];
+    const [ct1,setCt1]=useState("")
+    const [ct2,setCt2]=useState("")
+    const [ct3,setCt3]=useState("")
+    const [categories,setCategories]=useState([])
+    const [secCategories,setSecCategories]=useState([])
+    const [thirdCategories,setThirdCategories]=useState([])
+    const getCategories=async()=>{
+        let result=await reqCategories()
+        var list=[]
+        result.data.forEach(item=>{
+            list.push(<Option key={item['uuid']}>{item['name']}</Option>);
+        })
+        setCategories(list)
+    }
+    useEffect(()=>{getCategories()},[])
+    const handleCategoryChange=async(key)=>{
+        if (ct1!=key){
+            props.form.resetFields(['ct2','ct3'])
+            setSecCategories([])
+            setThirdCategories([])
+        }
+        setCt1(key)
+        let result=await reqSecCategories(key)
+        var list=[]
+        result.data.children.forEach(item=>{
+            list.push(<Option key={item['uuid']}>{item['name']}</Option>);
+        })
+        setSecCategories(list)
+    }
+    const handleSecCategoryChange=async(key)=>{
+        if (ct2!=key){
+            props.form.resetFields(['ct3'])
+            setThirdCategories([])
+        }
+        setCt2(key)
+        let result=await reqThirdCategories(ct1,key)
+        var list=[]
+        result.data.children.forEach(item=>{
+            list.push(<Option key={item['uuid']}>{item['name']}</Option>);
+        })
+        setThirdCategories(list)
+    }
+    return (
+      <Form 
+        {...formItemLayout}
+        form={props.form} 
+        name="editspu" 
+        scrollToFirstError
+        initialValues={{valid:true,saleable:true}}
+      >
+        <Item name="name" 
+          label="SPU Name" 
+          tooltip="SPU Name"
+          rules={[{required:true,message:"Please input spu name",whitespace:true,}]}
+        >
+            <Input/>
+        </Item>
+        <Item
+            name="ct1"
+            label="First Category"
+        >
+            <Select onChange={handleCategoryChange} placeholder="select your first category">
+                {categories}
+            </Select>
+        </Item>
+        <Item
+            name="ct2"
+            label="Second Category"
+        >
+            <Select onChange={handleSecCategoryChange} placeholder="select your second category">
+                {secCategories}
+            </Select>
+        </Item>
+        <Item
+            name="ct3"
+            label="Third Category"
+        >
+            <Select placeholder="select your third category">
+                {thirdCategories}
+            </Select>
+        </Item>
+        <Item name="valid" tooltip="true:valid  false:deleted" label="Is Valid" valuePropName="checked">
+            <Checkbox />
+        </Item>
+        <Item name="saleable" tooltip="true: onshore  false:offshore" label="Is Saleable" valuePropName="checked">
+            <Checkbox />
+        </Item>
+        <Item
+            name="description"
+            label="Description"
+            rules={[
+              {
+                message: 'please enter spu description',
+              },
+            ]}
+        >
+            <Input.TextArea rows={3} maxLength={300} showCount placeholder="please enter url description" />
+        </Item>
+        <Item
+            name="productdetails"
+            label="Product Details"
+            rules={[
+              {
+                message: 'please enter spu product details',
+              },
+            ]}
+        >
+            <Input.TextArea rows={5} maxLength={1000} showCount placeholder="please enter url description" />
+        </Item>
+        <Item
+            name="featureandbenefits"
+            label="Feature & Benefits"
+            rules={[
+              {
+                message: 'please enter feature & benefits',
+              },
+            ]}
+        >
+            <Input.TextArea rows={5} maxLength={3000} showCount placeholder="please enter url description" />
+        </Item>
+          <Form.List name="spectemplate">
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map(({ key, name, ...restField }) => (
+                <Space key={key} className="spu-select"align="baseline">
+                  <Item
+                    {...restField}
+                    name={[name, 'key']}
+                    rules={[{ required: true, message: 'Missing Spec Key Words' }]}
+                    className="select-key"
+                  >
+                      <Input placeholder="Spec Key" />
+                  </Item>
+                  <Item
+                    {...restField}
+                    name={[name, 'values']}
+                    rules={[{ required: true, message: 'Missing Spec Values' }]}
+                    className="select-value"
+                  >
+                      <Select mode="tags" placeholder="Spec Values">
+                          {children}
+                      </Select>
+                  </Item>
+                  <MinusCircleOutlined onClick={() => remove(name)} className="select-minus"/>
+                </Space>
+              ))}
+              <Item>
+                <Button type="dashed" style={{marginLeft:140}} onClick={() => add()} block icon={<PlusOutlined />}>
+                  Add Spec Values
+                </Button>
+              </Item>
+            </>
+          )}
+        </Form.List>
+      </Form>
+    );
+};
